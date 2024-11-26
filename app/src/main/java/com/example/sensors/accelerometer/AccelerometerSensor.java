c320279d2bdb87d56872ae933200354ef31aafc6
rajnish9651 <rajanish.singh@celestialsys.com>
2024-11-26 22:42:35 +0530

    2 1

--- app/src/main/java/com/example/sensors/accelerometer/AccelerometerSensor.java
+++ app/src/main/java/com/example/sensors/accelerometer/AccelerometerSensor.java
@@ -14,7 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.sensors.R;
 
 
-//this accelerometer sensor check 2
+//this accelerometer sensor check 2 1
 public class AccelerometerSensor extends AppCompatActivity implements SensorEventListener {
 
     TextView text;
